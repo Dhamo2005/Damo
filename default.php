cb80ff79c2ed177b99a0724c7fd3ccca35045f5d
Dhamo2005 <73931600+Dhamo2005@users.noreply.github.com>
2021-11-05 08:08:48 +0530

    c

--- default.php
+++ default.php
@@ -10,8 +10,8 @@ function activer($act, $additional_text)
         echo $additional_text;
     }
 }?>
-<!--<link rel="stylesheet" href="assets/material-icon-font-pack-master/sass/material-icons-outlined.css">-->
- <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet"> 
+<link rel="stylesheet" href="assets/material-icon-font-pack-master/sass/material-icons-outlined.css">
+ <!-- <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">  -->
 <html lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
